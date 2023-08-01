31e87d07dc076d30818bdd072556d20b9b79e209
Ian Scott <scottianw@gmail.com>
2023-07-31 20:15:56 -0400

    Another minor tweak to satisfy Prettier

--- invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/api/index.js
+++ invenio_rdm_records/assets/semantic-ui/js/invenio_rdm_records/src/deposit/api/index.js
@@ -9,6 +9,7 @@ export { DepositApiClient } from "./DepositApiClient";
 
 export { DepositFormApp } from "./DepositFormApp";
 export { DepositRecordSerializer } from "./DepositRecordSerializer";
-export { DepositFormSubmitContext,
-         DepositFormSubmitActions
+export {
+    DepositFormSubmitContext,
+    DepositFormSubmitActions
 } from "./DepositFormSubmitContext";
